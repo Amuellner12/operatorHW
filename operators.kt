bd7b5b8ed08457b178793c97d7cd044ea3e2643d
ALEKZANDER MUELLNER <alekz.muellner24@CLS-APP-07.local>
2022-08-22 08:50:38 -0500

    Initial commit

---
+++ operators.kt
@@ -0,0 +1,25 @@
+fun main() {
+    {
+
+        var sum: Double
+        var diff: Int
+        var quotient: Double
+        var product: Double
+        var greatertThanLessThan: Boolean
+
+        var x=6870.78
+        var y=198.9
+
+        sum = x+y
+        println(sum)
+        diff=x.toInt()-y.toInt()
+        println(diff)
+        quotient = x/y
+        println(quotient)
+        product = x*y
+        println(product)
+        greatertThanLessThan = x<=y
+        println(greatertThanLessThan)
+
+    }
+}
